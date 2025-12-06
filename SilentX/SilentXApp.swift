6a46f21db97848a13224228b441d38b1f96f8d27
TensorNull <tensor.null@gmail.com>
2025-12-06 10:43:30 +0800

    Initial Commit

---
+++ SilentX/SilentXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilentXApp.swift
+//  SilentX
+//
+//  Created by xmx on 6/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct SilentXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SilentXTests/SilentXTests.swift
@@ -0,0 +1,17 @@
+//
+//  SilentXTests.swift
+//  SilentXTests
+//
+//  Created by xmx on 6/12/2025.
+//
+
+import Testing
+@testable import SilentX
+
+struct SilentXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
